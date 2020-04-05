7f644eff9197584dd75c49987ed45bb107d1f2e6
Vijaya Raghavendra <35492336+raghavendraivv@users.noreply.github.com>
2020-04-06 02:40:48 +0530

    Update sentiment.py

--- sentiment.py
+++ sentiment.py
@@ -1,9 +1,9 @@
 import twitter
 
-consumer_key = "0PUaJu0HnaUopQj9cAk8qO86V"
-consumer_secret = "RtBy0xyTJTaGm4Ck7JRDnhja2GBDyr8q5PSdrWiIpaBxMCUHID"
-access_key = "1341421482-QlzsdzLRE2ijEGajPjE48SAIbmya5NRobW4cSQn"
-access_secret = "sqOFIG9J6lTrTy5Q763uXpNP25GvkFQj2BWSvuNYPjSyN"
+consumer_key = "-"
+consumer_secret = "-"
+access_key = "-"
+access_secret = "-"
 
 auth = twitter.oauth.OAuth(access_key,access_secret,consumer_key,consumer_secret)
 
