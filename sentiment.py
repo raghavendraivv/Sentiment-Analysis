b57966d34975575a703df95725b02635ad7aff76
raghavendraivv <ivvraghavendra1999@gmail.com>
2020-04-20 23:42:23 +0530

    #1

--- sentiment.py
+++ sentiment.py
@@ -1,4 +1,7 @@
 import twitter
+import csv
+import tweepy
+import pandas as pd
 
 consumer_key = "0PUaJu0HnaUopQj9cAk8qO86V"
 consumer_secret = "RtBy0xyTJTaGm4Ck7JRDnhja2GBDyr8q5PSdrWiIpaBxMCUHID"
@@ -8,9 +11,18 @@ access_secret = "sqOFIG9J6lTrTy5Q763uXpNP25GvkFQj2BWSvuNYPjSyN"
 auth = twitter.oauth.OAuth(access_key,access_secret,consumer_key,consumer_secret)
 
 twitter_api = twitter.Twitter(auth=auth)
-
+api = tweepy.API(auth,wait_on_rate_limit=True)
 trend = twitter_api.trends.place(_id=23424848)
 
 for i in trend[0]['trends']:
     r = i['name'],'   ', i['tweet_volume']
     print(r)
+
+x=input()
+csvFile = open('ua.csv', 'a')
+try:
+    print("Correct")
+    feteched = twitter_api.GetSearch(x, count = 100)
+    print(feteched + x)
+except:
+    print("Wrong")
